3bf8a83aab94931a590643e377ff294778a62b0c
mtstechnologies <michael.trindade@hotmail.com>
2022-05-25 01:35:02 -0400

    Projeto criado

---
+++ p005-filmes-api/src/test/java/com/mts/p005filmesapi/P005FilmesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mts.p005filmesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P005FilmesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
